b88a44ca312956c585b623f50f30baf188bb2334
Risabh Rai <risabhrai2001@gmail.com>
2022-11-03 11:12:49 +0530

    strict search added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const Contact = require("./Contact");
 const ContactManager = require("./ContactManager");
 
+const strictSearch = (key, param) => {
+    const searchResults = contactBook.filter(contact => contact[param] === key)
+    return searchResults
+}
