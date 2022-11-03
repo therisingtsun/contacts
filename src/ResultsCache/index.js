22baf33926dfc0c6a0d95d483fb197881e00cf56
Risabh Rai <risabhrai2001@gmail.com>
2022-11-03 14:37:52 +0530

    put method

--- src/ResultsCache/index.js
+++ src/ResultsCache/index.js
@@ -1,28 +1,36 @@
 const Contact = require("../Contact");
 
 class ResultsCache {
-	/**
-	 * @param {number} size 
-	 */
-	constructor (size) {
-		this.cache = new Map();
-		this.size = size;
-	}
+    /**
+     * @param {number} size 
+     */
+    constructor(size) {
+        this.cache = new Map();
+        this.size = size;
+    }
 
-	/**
-	 * @param {string} key 
-	 */
-	retrieve(key) {
+    /**
+     * @param {string} key 
+     */
+    retrieve(key) {
 
-	}
+    }
 
-	/** 
-	 * @param {string} key 
-	 * @param {Array<Contact>} value 
-	 */
-	put(key, value) {
+    /** 
+     * @param {string} key 
+     * @param {Array<Contact>} value 
+     */
+    put(key, value) {
+        this.cache.delete(key);
 
-	}
+        if (this.cache.size === this.size) {
+            this.cache.delete(this.cache.keys().next().value);
+            this.cache.set(key, value);
+        }
+        else {
+            this.cache.set(key, value);
+        }
+    }
 }
 
 module.exports = ResultsCache;
