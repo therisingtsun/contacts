c0122d2bdbb2baef95e7431a92245e3d97fcff81
Risabh Rai <risabhrai2001@gmail.com>
2022-11-03 11:35:19 +0530

    merged with class

--- src/ContactManager/index.js
+++ src/ContactManager/index.js
@@ -1,35 +1,47 @@
 const Contact = require("../Contact");
 
 class ContactManager {
-	constructor() {
-		this.contacts = [];
-	}
+    constructor() {
+        this.contacts = [];
+    }
 
-	/**
-	 * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
-	 * @param {string | number} key 
-	 */
-	searchPartial(keyType, key) {
-		const results = this.contacts.filter(
-			(contact) => new RegExp(`^${key}.*`)
-				.test(contact[keyType].toString())
-		);
-		
-		return {
-			count: results.length,
-			results
-		};
-	}
+    /**
+     * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
+     * @param {string | number} key 
+     */
+    partialSearch(keyType, key) {
+        const results = this.contacts.filter(
+            (contact) => new RegExp(`^${key}.*`)
+                .test(contact[keyType].toString())
+        );
 
-	/**
-	 * @param {string} firstName 
-	 * @param {string} lastName 
-	 * @param {number} phoneNumber 
-	 */
-	addContacts(firstName, lastName, phoneNumber) {
-		const newContact = new Contact(firstName, lastName, phoneNumber)
-		this.contacts.push(newContact)
-	}
+        return {
+            count: results.length,
+            results
+        };
+    }
+
+    /**
+     * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
+     * @param {string | number} key 
+     */
+    strictSearch(keyType, key) {
+        const results = contactBook.filter(contact => contact[keyType].toString() === key)
+        return {
+            count: results.length,
+            results
+        };
+    }
+
+    /**
+     * @param {string} firstName 
+     * @param {string} lastName 
+     * @param {number} phoneNumber 
+     */
+    addContacts(firstName, lastName, phoneNumber) {
+        const newContact = new Contact(firstName, lastName, phoneNumber)
+        this.contacts.push(newContact)
+    }
 }
 
 module.exports = ContactManager;
