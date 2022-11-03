9944b3e68f2845dfd964c4ff8c27f04a89f7af96
Vipin Kumar Yadav <62796627+vipiny357@users.noreply.github.com>
2022-11-03 11:45:21 +0530

    search function

--- src/ContactManager/index.js
+++ src/ContactManager/index.js
@@ -1,47 +1,55 @@
 const Contact = require("../Contact");
-
+const strategies = { }
 class ContactManager {
-    constructor() {
-        this.contacts = [];
-    }
+	constructor() {
+		this.contacts = [];
+	}
 
-    /**
-     * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
-     * @param {string | number} key 
-     */
-    partialSearch(keyType, key) {
-        const results = this.contacts.filter(
-            (contact) => new RegExp(`^${key}.*`)
-                .test(contact[keyType].toString())
-        );
+	/**
+	 * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
+	 * @param {string | number} key 
+	 */
+	partialSearch(keyType, key) {
+		const results = this.contacts.filter(
+			(contact) => new RegExp(`^${key}.*`)
+				.test(contact[keyType].toString())
+		);
 
-        return {
-            count: results.length,
-            results
-        };
-    }
+		return {
+			count: results.length,
+			results
+		};
+	}
+	search(keyType, key, partial = false) {
 
-    /**
-     * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
-     * @param {string | number} key 
-     */
-    strictSearch(keyType, key) {
-        const results = contactBook.filter(contact => contact[keyType].toString() === key)
-        return {
-            count: results.length,
-            results
-        };
-    }
+		const results = contactBook.filter(contact => (partial ? this.partialSearch : this.strictSearch).call(null,contact,keyType,key))
+		return {
+			count: results.length,
+			results
+		};
+	}
 
-    /**
-     * @param {string} firstName 
-     * @param {string} lastName 
-     * @param {number} phoneNumber 
-     */
-    addContacts(firstName, lastName, phoneNumber) {
-        const newContact = new Contact(firstName, lastName, phoneNumber)
-        this.contacts.push(newContact)
-    }
+	/**
+	 * @param {"firstName" | "lastName" | "phoneNumber"} keyType 
+	 * @param {string | number} key 
+	 */
+	strictSearch(keyType, key) {
+		const results = contactBook.filter(contact => contact[keyType].toString() === key)
+		return {
+			count: results.length,
+			results
+		};
+	}
+
+	/**
+	 * @param {string} firstName 
+	 * @param {string} lastName 
+	 * @param {number} phoneNumber 
+	 */
+	addContacts(firstName, lastName, phoneNumber) {
+		const newContact = new Contact(firstName, lastName, phoneNumber)
+		this.contacts.push(newContact)
+	}
 }
 
 module.exports = ContactManager;
