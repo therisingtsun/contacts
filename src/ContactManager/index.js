dfb4305fc11560a55cc4ca196010f398a2184757
therisingtsun <77495103+therisingtsun@users.noreply.github.com>
2022-11-03 11:22:13 +0530

    partial search

--- src/ContactManager/index.js
+++ src/ContactManager/index.js
@@ -1,7 +1,24 @@
+const Contact = require("../Contact");
 
 class ContactManager {
 	constructor () {
 		this.contacts = [];
+	}
+
+	/**
+	 * @param {"firstName" | "lastName", "phoneNumber"} keyType 
+	 * @param {string | number} key 
+	 */
+	searchPartial(keyType, key) {
+		const results = this.contacts.filter(
+			(contact) => new RegExp(`^${key}.*`)
+				.test(contact[keyType].toString())
+		);
+		
+		return {
+			count: results.length,
+			results
+		};
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 const Contact = require("./Contact");
 const ContactManager = require("./ContactManager");
 
+const manager = new ContactManager();
+
+const contactA = new Contact("Vipin", "Yadav", 1234567890);
+const contactB = new Contact("Vipin", "Changed", 1122334455);
+const contactC = new Contact("Risabh", "Yadav", 8899889988);
+
+manager.contacts.push(contactA, contactB, contactC);
+
+// console.log(manager.searchPartial("phoneNumber", 1));
