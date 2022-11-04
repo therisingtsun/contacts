23c933db841ca95bd299329679b46cfb310352ad
Risabh Rai <risabhrai2001@gmail.com>
2022-11-04 09:39:59 +0530

    error handling

--- src/ContactManager/index.js
+++ src/ContactManager/index.js
@@ -1,5 +1,6 @@
 const crypto = require("node:crypto");
 const Trie = require("trie-search");
+const validator = require("validator")
 
 const Contact = require("../Contact");
 const ResultsCache = require("../ResultsCache");
@@ -7,59 +8,68 @@ const ResultsCache = require("../ResultsCache");
 /** @typedef {"firstName" | "lastName" | "phoneNumber"} keyType */
 
 class ContactManager {
-	constructor() {
-		this.contacts = [];
-		this.cachedResults = new ResultsCache();
-		this.tries = {
-			firstName: new Trie(),
-			lastName: new Trie(),
-			phoneNumber: new Trie()
-		};
-	}
+    constructor() {
+        this.contacts = [];
+        this.cachedResults = new ResultsCache();
+        this.tries = {
+            firstName: new Trie(),
+            lastName: new Trie(),
+            phoneNumber: new Trie()
+        };
+    }
 
-	static strategies = {
-		prefixSearch(manager, keyType, key) {
-			return manager.tries[keyType].search(key).map(index => manager.contacts[index]);
-		},
-		strictSearch(manager, keyType, key) {
-			return manager.contacts.filter(contact => contact[keyType].toLowerCase() === key.toLowerCase());
-		}
-	}
+    static strategies = {
+        prefixSearch(manager, keyType, key) {
+            return manager.tries[keyType].search(key).map(index => manager.contacts[index]);
+        },
+        strictSearch(manager, keyType, key) {
+            return manager.contacts.filter(contact => contact[keyType].toLowerCase() === key.toLowerCase());
+        }
+    }
 
-	/**
-	 * @param {keyType} keyType 
-	 * @param {string} key 
-	 * @param {string} searchStrategy 
-	 * @returns 
-	 */
-	search(keyType, key, searchStrategy) {
-		const secret = "CRED";
-		const hash = crypto.createHmac("sha256", secret)
-			.update(keyType + key + searchStrategy)
-			.digest("hex");
-		let results = this.cachedResults.retrieve(hash);
-		if (!results) {
-			results = ContactManager.strategies[searchStrategy](this, keyType, key);
-			this.cachedResults.put(hash, results);
-		}
-		return {
-			count: results.length,
-			results
-		};
-	}
+    /**
+     * @param {keyType} keyType 
+     * @param {string} key 
+     * @param {string} searchStrategy 
+     * @returns 
+     */
+    search(keyType, key, searchStrategy) {
+        const secret = "CRED";
+        const hash = crypto.createHmac("sha256", secret)
+            .update(keyType + key + searchStrategy)
+            .digest("hex");
+        let results = this.cachedResults.retrieve(hash);
+        if (!results) {
+            results = ContactManager.strategies[searchStrategy](this, keyType, key);
+            this.cachedResults.put(hash, results);
+        }
+        return {
+            count: results.length,
+            results
+        };
+    }
 
-	/**
-	 * @param {string} firstName 
-	 * @param {string} lastName 
-	 * @param {string} phoneNumber 
-	 */
-	addContact(firstName, lastName, phoneNumber) {
-		this.tries.firstName.map(firstName, this.contacts.length);
-		this.tries.lastName.map(lastName, this.contacts.length);
-		this.tries.phoneNumber.map(phoneNumber, this.contacts.length);
-		this.contacts.push(new Contact(firstName, lastName, phoneNumber));
-		return this;
-	}
+    /**
+     * @param {string} firstName 
+     * @param {string} lastName 
+     * @param {string} phoneNumber 
+     */
+    addContact(firstName, lastName, phoneNumber) {
+
+        if (!firstName.length || !phoneNumber.length) throw new Error("Input can't be NULL")
+        if (!(/^\+[1-9]\d{1,14}$/).test(phoneNumber)) throw new Error("Phone Number should be a valid input")
+
+        if (firstName.length > 100) throw new Error("First Name length exceeds limit")
+        if (lastName.length > 100) throw new Error("Last Name length exceeds limit")
+        if (phoneNumber.length > 15) throw new Error("Phone Number length exceeds limit")
+
+        const index = this.contacts.length;
+        this.tries.firstName.map(firstName, index);
+        this.tries.lastName.map(lastName, index);
+        this.tries.phoneNumber.map(phoneNumber, index);
+        this.contacts.push(new Contact(firstName, lastName, phoneNumber));
+        return this;
+    }
 }
 
 module.exports = ContactManager;
